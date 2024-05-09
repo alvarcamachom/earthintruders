2d9c64169d1820640e8e0804373e01239a56f7a8
alvarcamachom <alvar.camacho@gmail.com>
2024-05-10 00:38:49 +0200

    hubij

--- api/message/index.js
+++ api/message/index.js
@@ -1,22 +1,66 @@
-const React = require('react');
-const ReactDOMServer = require('react-dom/server');
+const express = require('express')
+const app = express()
+const port = 3001
 
-function MyComponent() {
-    return (
-        <div style={{ textAlign: 'center', marginTop: '50px', fontSize: '24px' }}>
-            <p>Hola que tal</p>
-        </div>
+const Pool = require('pg').Pool
+const pool = new Pool({
+  user: '',
+  host: 'spaceinvadersdatos.postgres.database.azure.com',
+  database: '',
+  password: '',
+  port: 5432,
+});
+
+app.use(express.json())
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://spaceinvadersdatos.postgres.database.azure.com');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
+  next();
+});
+
+app.use(function (req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Content-Type, Access-Control-Allow-Headers"
     );
-}
+    next();
+  });
 
-module.exports = async function (context, req) {
-    const html = ReactDOMServer.renderToString(<MyComponent />);
-    
-    context.res = {
-        status: 200,
-        headers: {
-            'Content-Type': 'text/html'
-        },
-        body: html
-    };
-};
+  const getMerchants = async () => {
+    try {
+      return await new Promise(function (resolve, reject) {
+        pool.query("SELECT * FROM ranking", (error, results) => {
+          if (error) {
+            reject(error);
+          }
+          if (results && results.rows) {
+            resolve(results.rows);
+          } else {
+            reject(new Error("No results found"));
+          }
+        });
+      });
+    } catch (error_1) {
+      console.error(error_1);
+      throw new Error("Internal server error");
+    }
+  };
+
+app.get('/', (req, res) => {
+  getMerchants()
+  .then(response => {
+    res.status(200).send(response);
+    console.log("ejecutado")
+  })
+  .catch(error => {
+    res.status(500).send(error);
+    console.log("error")
+  })
+})
+
+app.listen(port, () => {
+    console.log(`App running on port ${port}.`)
+  })
--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
-  const [data, setData] = useState('');
+  const [merchants, setMerchants] = useState([]);
 
   useEffect(() => {
-    (async function () {
-      const { text } = await( await fetch(`/api/message`)).json();
-      setData(text);
-    })();
-  });
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:3001');
+        setMerchants(response.data);
+        console.log("Data fetched successfully");
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
 
-  return <div>{data}</div>;
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>Merchants</h1>
+      <ul>
+        {merchants.map(merchant => (
+          <li key={merchant.id}>{merchant.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
