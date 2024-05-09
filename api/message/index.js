a3ae808ce7b9498922dff164dc8f58c5cf046b51
alvarcamachom <alvar.camacho@gmail.com>
2024-05-10 00:55:15 +0200

    fff

--- api/message/index.js
+++ api/message/index.js
@@ -20,7 +20,7 @@ app.use(function (req, res, next) {
 });
 
 app.use(function (req, res, next) {
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3001");
     res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
     res.setHeader(
       "Access-Control-Allow-Headers",
