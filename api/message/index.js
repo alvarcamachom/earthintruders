2c29d3c1831a331a97efc927e2a8206750207670
alvarcamachom <alvar.camacho@gmail.com>
2024-05-10 00:22:57 +0200

    prueba

--- api/message/index.js
+++ api/message/index.js
@@ -1,5 +1,22 @@
+const React = require('react');
+const ReactDOMServer = require('react-dom/server');
+
+function MyComponent() {
+    return (
+        <div style={{ textAlign: 'center', marginTop: '50px', fontSize: '24px' }}>
+            <p>Hola que tal</p>
+        </div>
+    );
+}
+
 module.exports = async function (context, req) {
-    context.res.json({
-        text: "Hello from the API"
-    });
+    const html = ReactDOMServer.renderToString(<MyComponent />);
+    
+    context.res = {
+        status: 200,
+        headers: {
+            'Content-Type': 'text/html'
+        },
+        body: html
+    };
 };
